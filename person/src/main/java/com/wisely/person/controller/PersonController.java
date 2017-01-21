c142ee8f7d870e4b12c26ac9c26ecfeaffa6e9cc
yushi <yushiwh@126.com>
2017-01-21 18:28:31 +0800

    修改成自定义的条数的

--- person/src/main/java/com/wisely/person/controller/PersonController.java
+++ person/src/main/java/com/wisely/person/controller/PersonController.java
@@ -1,10 +1,10 @@
 package com.wisely.person.controller;
 
-
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,17 +15,21 @@ import com.wisely.person.domain.Person;
 
 @RestController
 public class PersonController {
-    @Autowired
-    PersonRepository personRepository;
+	@Autowired
+	PersonRepository personRepository;
 
-    @RequestMapping(value = "/save", method = RequestMethod.POST)
-    public List<Person> savePerson(@RequestBody String  personName) {
-    	Person p = new Person(personName);
-    	personRepository.save(p);
-    	List<Person> people = personRepository.findAll(new PageRequest(0, 10)).getContent();
-        return people;
-    }
+	@RequestMapping(value = "/save/{count}", method = RequestMethod.POST)
+	public List<Person> savePerson(@RequestBody String personName, @PathVariable("count") int count) {
+		Person p = new Person(personName);
+		personRepository.save(p);
+		// List<Person> people = personRepository.findAll(new PageRequest(0,
+		// 10)).getContent();
+		if (count == 0) {
+			count = 10;
+		}
+		List<Person> people = personRepository.findAll(new PageRequest(0, count)).getContent();// 换成自定义
 
-
+		return people;
+	}
 
 }
