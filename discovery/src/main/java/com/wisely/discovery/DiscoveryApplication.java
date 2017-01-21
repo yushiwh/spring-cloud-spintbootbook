0d88adbd89153a392005befd24e133f965be4d99
yushi <yushiwh@126.com>
2017-01-21 16:03:57 +0800

    增加discovery的eureka的server的开启的注解

--- discovery/src/main/java/com/wisely/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/wisely/discovery/DiscoveryApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer //1
+@EnableEurekaServer // 1一个常规的springboot启动项目，开启@EnableEurekaServer注解，对EurekaServer的支持
 public class DiscoveryApplication {
 
-	  public static void main(String[] args) {
-	        SpringApplication.run(DiscoveryApplication.class, args);
-	    }
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
 
 }
