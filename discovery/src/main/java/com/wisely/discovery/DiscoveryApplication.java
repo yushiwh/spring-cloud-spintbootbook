a3c0ab2bb19226dd01f91d54531192e8d53c11da
yushi <yushiwh@126.com>
2017-02-05 09:22:53 +0800

    测试一下看能不能提交

--- discovery/src/main/java/com/wisely/discovery/DiscoveryApplication.java
+++ discovery/src/main/java/com/wisely/discovery/DiscoveryApplication.java
@@ -8,6 +8,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer // 1一个常规的springboot启动项目，开启@EnableEurekaServer注解，对EurekaServer的支持
 public class DiscoveryApplication {
 
+
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryApplication.class, args);
 	}
